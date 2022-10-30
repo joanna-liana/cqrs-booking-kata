a14996e00915e086bbf563c25bf810007c946dce
Joanna Liana <joanna_liana@outlook.com>
2022-10-30 15:02:01 +0100

    Add separate Wallaby configs

---
+++ wallaby.it.js
@@ -0,0 +1,19 @@
+module.exports = function (_wallaby) {
+  return {
+    tests: {
+      override: (_testPatterns) => {
+        return ['tests/integration/**/*.test.ts'];
+      }
+    },
+
+    env: {
+      type: 'node'
+    },
+
+    testFramework: {
+      configFile: 'jest.it.config.js'
+    },
+
+    // trace: true
+  };
+};
