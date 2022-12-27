73b6d8bc2518330bb25dce7415e705fb255a06cd
Joanna Liana <joanna_liana@outlook.com>
2022-12-27 15:23:16 +0100

    Add logs to E2E script

--- express-e2e.sh
+++ express-e2e.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 yarn install && \
+echo "Installed root deps" && \
 yarn docker:test && \
+echo "Started test docker" && \
 cd express && yarn install && RABBIT_PORT=5673 PG_PORT=5435 PG_PASSWORD=postgres yarn start & sleep 5 && \
+echo "Installed Express deps & started the app" && \
+echo $PWD && \
 yarn test:e2e
