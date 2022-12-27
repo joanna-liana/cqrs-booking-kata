4da6e2767eae5c9a9e8e40a3e7cc03fa2b8f3027
Joanna Liana <joanna_liana@outlook.com>
2022-12-27 15:18:15 +0100

    Fix E2E script

--- express-e2e.sh
+++ express-e2e.sh
@@ -2,5 +2,5 @@
 
 yarn install && \
 yarn docker:test && \
-cd express && yarn install && RABBIT_PORT=5673 PG_PORT=5435 PG_PASSWORD=postgres yarn start & sleep 5 \
-cd .. && yarn test:e2e
+cd express && yarn install && RABBIT_PORT=5673 PG_PORT=5435 PG_PASSWORD=postgres yarn start & sleep 5 && \
+yarn test:e2e
