d08522c2d06322bb921344f7c61b47ae01ceff94
Joanna Liana <joanna_liana@outlook.com>
2022-12-27 15:28:10 +0100

    Move root deps install step

--- express-e2e.sh
+++ express-e2e.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-yarn install && \
-echo "Installed root deps" && \
 yarn docker:test && \
 echo "Started test docker" && \
 cd express && yarn install && RABBIT_PORT=5673 PG_PORT=5435 PG_PASSWORD=postgres yarn start & sleep 5 && \
 echo "Installed Express deps & started the app" && \
 echo $PWD && \
-yarn test:e2e
+yarn install && yarn test:e2e
