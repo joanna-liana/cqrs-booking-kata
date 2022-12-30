ce93b4c7ee081524fa883a7d8795b05cecd2922d
Joanna Liana <joanna_liana@outlook.com>
2022-12-30 07:21:54 +0100

    Make injection tokens consistent

--- nest/src/bookings/injectionTokens.ts
+++ nest/src/bookings/injectionTokens.ts
@@ -1,4 +1,4 @@
 export const BOOKINGS_EVENT_BUS = 'BOOKINGS_EVENT_BUS';
 export const FREE_ROOM_FINDER = 'FREE_ROOM_FINDER';
-export const READ_REGISTRY = 'ReadRegistry';
-export const WRITE_REGISTRY = 'WriteRegistry';
+export const READ_REGISTRY = 'READ_REGISTRY';
+export const WRITE_REGISTRY = 'WRITE_REGISTRY';
